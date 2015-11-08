149dc52f3bec1ad640dd0cd5a48fc7305a55d729
andigernes <andigernes@gmail.com>
2015-11-08 20:38:46 +0100

    uteliv.php oppdatert

--- Websider/Uteliv.php
+++ Websider/Uteliv.php
@@ -12,16 +12,76 @@
 
 		<div class="backgroundImage">
 			<div class="content">
-				<h2> Uteliv </h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h1> Uteliv </h1>
+				<div id="Jacob_logo"></div>
+				<h3 id="Jacob_Aall"> Jacob Aall Restaurant og Bar: </h3>
+				<p id="Jacob_text">På toppen av Bergens flotteste kjøpesenter, Xhibition,
+				 omgitt av vakre Bergen ligger Jacob Aall Brasserie og Bar Bergen. 
+				 Restauranten er perfekt for å kombinere shoppingen med en god lunsj.
+				 Store vindusflater lar deg nyte utsikten over Bergen mens dere koser 
+				 dere med god mat og drikke. Ønsker du å nyte måltidet og Bergen til det fulle,
+				 har vi også en stor helårsåpen takterrasse.
+				 Her har vi egen utebar hvor du kan sitte i våre lounger og nyte god drikke i hyggelig
+				 selskap i behagelige omgivelser. 
+				 Det er mye folk innom hele dagen, og vi har etter hvert etablert oss som et 
+				 av Bergens største og mest populære utesteder. 
+				 Vi har også en egen barnemeny for å mette små mager. </p>
+				<div id="Jacob_images">
+					<div id="Jacob_Aall_pic1"></div>
+					<div id="Jacob_Aall_pic2"></div>
+					<div id="Jacob_Aall_pic3"></div>
+				</div>
+
+				<div id="Metro_content">
+					<div id="Metro_logo"></div>
+					<h3 id="Metro_id"> Metro: </h3>
+					<p id="Metro_text">Metro nightclub er et utested med internasjonal eksklusiv standard plassert 
+					midt i Bergen sentrum. Vi hadde vårt 10 års jubileum i 2011. 
+					Metro har hatt fokus på alltid å ligge lengst mulig fremme når det kommer til 
+					design og trend, og har derfor vært oppgradert jevnlig, senest i 2011.
+					På Metro er vi stolt over å ha det beste personalet, og ikke minst det beste klientellet 
+					Bergen har å tilby. Våre resident DJs er blant de beste i Skandinavia, og er oppdatert 
+					på det nyeste innen de rette musikksjangerene.</p>
+					<div id="Metro_images">
+						<div id="Metro_pic1"></div>
+						<div id="Metro_pic2"></div>
+						<div id="Metro_pic3"></div>
+					</div>
+				</div>
+
+				<div id="Lysverket_content">
+					<div id="Lysverket_logo"></div>
+					<h3 id="Lysverket_id"> Lysverket: </h3>
+					<p id="Lysverket_text">Lysverket er en restaurant, bar og nattklubb etablert i 2013 på kunstmuseet 
+					KODE 4 ved Grieghallen - midt i hjertet av Bergen med utsikt over Rasmus Meyers Allé, Lille Lungegårdsvannet, 
+					Festplassen, fjellsiden og Fløyen.
+					Som spisested er Lysverket en tydelig bergensrestaurant med fokus på fersk sjømat - 
+					men også kjøtt og rene grønnsaksretter står på menyen. Lysverket byr blant annet på de beste råvarene fra nærområdet 
+					såsom sjøkreps, hummer, kamskjell og østers i verdensklasse, og vi serverer både 4 og 7 retter i restauranten samt á 
+					la carte i barområdet. </p>
+					<div id="Lysverket_images">
+						<div id="Lysverket_pic1"></div>
+						<div id="Lysverket_pic2"></div>
+						<div id="Lysverket_pic3"></div>
+					</div>
+				</div>
+
+				<div id="FjordFusion_content">
+					<div id="FjordFusion_logo"></div>
+					<h3 id="FjordFusion_id"> Fjord Fusion: </h3>
+					<p id="FjordFusion_text">Fjord Fusion Restaurant og Bar ligger i 2 etg på Zachariasbryggen. 
+						Vi ligger i lokalene til tidligere Bryggekanten Restaurant og Sun Z Bar –  Vi serverer 
+						CONTEMPORARY SPANISH og JAPANESE CUISINE – med ny meny, kjøkkensjef og restaurantsjef ønsker 
+						vi deg velkommen til nyoppussede lokaler med Bergens desidert beste utsikt over Fisketorget og Vågen i Bergen </p>
+					<div id="FjordFusion_images">
+						<div id="FjordFusion_pic1"></div>
+						<div id="FjordFusion_pic2"></div>
+						<div id="FjordFusion_pic3"></div>
+					</div>
+				</div>
+
 			</div>
 		</div>
-
 		<footer>
 			<?php include 'footer.php';  ?>
 		</footer>
