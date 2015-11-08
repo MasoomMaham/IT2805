de9981f339da7565c1f5280b7939874ba86c6295
Masoom Maham <masoom_93@hotmail.com>
2015-11-08 10:20:42 +0100

    Template for the website

---
+++ Websider/header.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" type="text/css" property="stylesheet" href="../css/header.css">
+<div class="banner">
+	<a href="Index.php">
+		<div class="logo">
+		</div>
+	</a>
+	<div class="dropdown">
+		<ul>
+			<li><a href="Om_Bergen.php"> Om Bergen</a></li>
+			<li><a href="Attraksjoner.php">
+				Attraksjoner</a>
+				<ul>
+					<li><a href="Attraksjoner.php">Attraksjon 1</a> </li>
+					<li><a href="Attraksjoner.php"> Attraksjon 2</a> </li>
+				</ul>
+			</li>
+			<li><a href="Restaurant.php"> Restauranter</a>
+				<ul>
+					<li><a href="Restaurant.php"> Restaurant 1</a> </li>
+					<li><a href="Restaurant.php"> Restaurant 2</a> </li>
+				</ul>
+			</li>
+			<li><a href="Uteliv.php"> Uteliv</a>
+				<ul>
+					<li><a href="Uteliv.php">Utested 1</a> </li>
+					<li><a href="Uteliv.php"> Utested 2</a> </li>
+				</ul>
+			</li>
+			<li><a href="Kart.php"> Kart</a></li>
+		</ul>
+	</div>
+</div>
