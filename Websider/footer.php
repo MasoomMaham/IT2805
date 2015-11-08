6b44293dd540eda17b1830f5258c1bef67a356c2
Masoom Maham <masoommaham@gmail.com>
2015-11-08 10:25:05 +0100

    Deleted files, uploaded wrong files

--- Websider/footer.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" type="text/css" property="stylesheet" href="../css/footer.css">
-<div class="footMenu">
-	<ul>
-		<li><a href="Om_oss.php"> Om Oss </a></li>
-		<li><a href="Kontakt_oss.php"> Kontakt Oss </a></li>
-		<li><a href="Sitemap.php"> Site Map </a></li>
-	</ul>
-</div>
