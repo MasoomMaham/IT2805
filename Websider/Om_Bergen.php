408cf570815416bb97611f47d82e781e653c7814
andigernes <andigernes@gmail.com>
2015-11-09 21:07:32 +0100

    Lagt inn innhold på om_Bergen.php og endret Css

--- Websider/Om_Bergen.php
+++ Websider/Om_Bergen.php
@@ -12,13 +12,79 @@
 
 		<div class="backgroundImage">
 			<div class="content">
-				<h2> Om Bergen </h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<h1> Om Bergen </h1>
+				<h3 id="historie"> Historie: </h3>
+				<p id="historie_p"> Bergens Historie strekker seg så langt 
+					tilbake som 1070 da den ble grunnlagt av Kong Olav Kyrre.
+					Men byen var bebod lenge før dette ettersom i Årstad bydel er fundamentet til en av Norges 
+					eldste kongsgårder funnet like ved siden av Statsarkivet i Bergen, ved foten av fjellet Ulriken. 
+					Nedenfor, ved Store Lungegårdsvann, ligger en gravhaug fra 400-tallet bevart midt i et borettslag.
+					<br/><br/>
+					I norrøn tid var Alrekstaðir navnet på området som nå heter Årstad. 
+					Alrekstad var en av de kongsgårdene langs den sørvestnorske kysten der Harald Hårfagre og hans etterfølgere
+					tok opphold på 900-tallet før det vokste frem byer i Norge. 
+					Etter slaget ved Fitjar på Stord omkring år 960 var kong Håkon den gode på vei til Alrekstad da han døde av banesår i Håkonshella.
+					Ifølge Snorre Sturlassons saga om Håkon den gode, etter slaget ved Fitjar:
+					<br/><br/>
+					Kong Håkon gikk ut på langskipet sitt og lot binde om det såret han hadde fått, men blodet rant 
+					slik av det at de ikke kunne få det stanset, og da det lei på dagen, ble kongen svakere; han sa da at han ville seie 
+					nordover til gården sin på Ålrekstad... Og etter litt døde kong Håkon der på hella, samme sted som han var født.
+					<br/><br/>
+					Som kongssete var Alrekstad et viktig utgangspunkt for byutviklingen i Bergen. 
+					Årstadgeilen som går fra studenthjemmet Alrek bratt ned til Store Lungegårdsvann, er Bergens eldste gateløp. 
+					Navnet på Store Lungegårdsvann kommer av Lungegården, eiendommen til Vincens Lunge. I middelalderen het vannet Alrekstadvågen etter 
+					kongsgården Ålrekstad.
+					<br/><br/>
+					Bergen (Bjørgvin) ble grunnlagt av Olav Kyrre i 1070 og overtok etter Trondheim (Nidaros) som hovedstad for Norgesveldet frem til 1314, 
+					da denne funksjonen ble flyttet til Oslo. På 1100-tallet var Bergen den folkerikeste byen i Norden. 
+					År 1300 regner man med at det kan ha vært ca. 7 000 innbyggere i Bergen, ca. 3 000 i Nidaros, ca. 2 000 Oslo og ca. 1 500 i Tønsberg. 
+					På denne tiden hadde mellomstore byer i Europa 5 000 – 10 000 innbyggere, og Bergen var den eneste norske byen av slik størrelsesorden. 
+					Den danske kulturhistoriker Troels-Lund[3] anser at den fremdeles på 1600-tallet var «afgjort Nordens største By». Han anslår at den hadde ca. 15 000 innbyggere, 
+					fulgt av København med 13 000, mens Trondheim, Norges nest største by, hadde ca. 5 000 innbyggere. I 1814 var Bergen storbyen som sendte fire 
+					representanter til Eidsvoll, mens Christiania (Oslo), Trondheim og Kristiansand sendte to hver. 
+					Men to år senere passerte Christiania Bergen, både i folketall og innflytelse.	
+					<br/><br/>
+					Fra 1300-tallet til ca. 1750 var byen en del av Hansaforbundet og dets handelsnettverk som omfattet hele det nordlige Europa, noe som Bryggen i Bergen minner om. 
+					Bergen var både eksporthavn og administrasjonsby for Vestlandet og Nord-Norge i mange hundre år. 
+					Nordlandshandelen var et viktig næringsgrunnlag for byens kjøpmenn.
+					<br/><br/>
+					Det er estimert at av en befolkning på rundt 10 000 i middelalderens Bergen, var kanskje så mye som en tredjedel utlendinger – de fleste sannsynligvis våpenføre unge menn tilsluttet Hansaforbundet. 
+					Hansaforbundet hadde fire hovedkontorer i byer som ikke var tilsluttet hansaforbundet, disse lå i London, Brugge, Bergen og Novgorod. Bergen var altså ingen hansaby, 
+					men sete for ett av fire hanseatiske Kontorer. Det fantes derimot utallige mindre kontorer spredt over hele Østersjø-området, men disse ble kalt faktorier. 
+					Det fantes også faktorier i Oslo og Tønsberg. Kontoret i Bergen håndterte hovedsakelig tørrfiskhandel. Kontorene i London og Brugge hadde sin hovedvirksomhet i tøy- og ullhandel, 
+					mens det fra Novgorod ble sendt ut mye pelsverk, rav, korn og tømmer.
+					<br/><br/>
+					Rådhusene i byen stammer fra den hanseatiske tiden. Det er to rådhus, og det eldste er fra 1500-tallet. 
+					Bystyremøtene holdes fortsatt i den samme salen hvor de har vært holdt siden 1561.
+					</p>
+
+				<h3 id="bryggen"> Bryggen: </h3>
+				<p id="bryggen_p">Bryggen i Bergen, også kjent som Tyskebryggen og Hansabryggen, omfatter den gamle hanseatiske trebebyggelsen i det sentrale Bergen. Den består av 61 fredede bygninger, 
+				og befinner seg på UNESCOs liste over verdens kulturarv.
+				Strøket, som også ble kalt Tyskebryggen fra 1857, grenser mot Bergenhus langs Øvregaten til Vetrlidsallmenningen, 
+				ned denne og tilbake langs Bryggens kaifront. Det omfatter da også de handelsgårdene av mur som ble bygget etter en regulering i 1901 og etter 
+				tegninger av arkitekt Jens Zetlitz Monrad Kielland. I den forbindelse rev en en rekke av tregårdene fra hansatiden, men eieren av den gården som i dag inneholder 
+				Hanseatisk museum ville ikke selge og derfor er denne bevart. 
+				I 2006 hadde Bryggen et besøkstall på 583 510.</p>
+
+				<h3 id="Fisketorget"> Fisketorget: </h3>
+				<p id="Fisketorget_p"> Torget i Bergen er et av Norges mest kjente og berømte utendørsmarkeder.
+				Det har en sjarmerende beliggenhet midt i hjerte av byen mellom vestlandske fjorder og de syv fjell. 
+				Her selges sjømat, frukt, grønnsaker, kunsthåndverk og suvenirer.
+				Torget i Bergen er et av Norges mest kjente og berømte utendørsmarkeder. 
+				Det har en sjarmerende beliggenhet midt i hjerte av byen mellom vestlandske fjorder og de syv fjell. 
+				Her selges sjømat, frukt, grønnsaker, kunsthåndverk og suvenirer. </p>
+
+				<h3 id="Trollhaugen"> Trollhaugen: </h3>
+				<p id="Trollhaugen_p"> Troldhaugen var hjemmet til Edvard Grieg fra april 1885 til han døde i 1907. Grieg bodde på Troldhaugen med sin kone,
+				Nina Hagerup når han var hjemme i Norge, for det meste om somrene. Asken etter ekteparet ligger i en grav inne i fjellet nede ved stranden på eiendommen.
+				Edvard Grieg kalte selv bygningen «mitt beste opus hittil», selv om den ble tegnet av hans fetter, arkitekten Schak August Steenberg Bull. Bygningen er tegnet i historisme. Den er en typisk trevilla fra det 19. århundre med panoramatårn og en stor veranda. 
+				Uvanlig for den tiden var de store vinduene, som Grieg ønsket for å få mye lys og luft inn i huset. Dette medførte at boligen var kald om vinteren, men på denne årstiden var ekteparet allikevel som oftest ute og reiste.
+				Det var Nina som foreslo navnet Troldhaugen, fordi huset ligger på haugen ved siden av Trolddalen.
+				I 1921 tegnet arkitekt Ole Landmark en ny bolig på Troldhaugen etter oppdrag av Einar Grieg, sønn av Edvard Griegs nevø, konsul Joachim Grieg. 
+				Landmarks forslag, som var i mur og minnet om baroniet i Rosendal,
+				forutsatte riving av Edvard Griegs Troldhaugen, men i stedet ble dette donert til Fana kommune av Joachim Grieg. Fra 1928 ble det museum, 
+				men Landmarks tegninger til det nye Troldhaugen er oppbevart hos Bergen byarkiv </p>
 			</div>
 		</div>
 
