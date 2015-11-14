b0da9687b49ccb22ddfaf75136c2c6cdedd6bc98
Masoom Maham <masoommaham@gmail.com>
2015-11-14 15:40:25 +0100

    Added names of restaurants and attractions

--- Websider/header.php
+++ Websider/header.php
@@ -9,15 +9,20 @@
 			<li><a href="Om_Bergen.php"> Om Bergen</a></li>
 			<li><a href="Attraksjoner.php">
 				Attraksjoner</a>
-				<ul>
-					<li><a href="Attraksjoner.php">Attraksjon 1</a> </li>
-					<li><a href="Attraksjoner.php"> Attraksjon 2</a> </li>
+				<ul	>
+					<li><a href="Attraksjoner.php">Akvariet</a> </li>
+					<li><a href="Attraksjoner.php"> Fløibanen</a> </li>
+					<li><a href="Attraksjoner.php"> Håkonshallen</a> </li>
+					<li><a href="Attraksjoner.php"> Bryggen</a> </li>
 				</ul>
 			</li>
 			<li><a href="Restaurant.php"> Restauranter</a>
 				<ul>
-					<li><a href="Restaurant.php"> Restaurant 1</a> </li>
-					<li><a href="Restaurant.php"> Restaurant 2</a> </li>
+					<li><a href="Restaurant.php"> Fisketorget</a> </li>
+					<li><a href="Restaurant.php"> 1877</a> </li>
+					<li><a href="Restaurant.php"> Horn of Africa</a> </li>
+					<li><a href="Restaurant.php"> Cornelius Sjømatrestaurant</a> </li>
+
 				</ul>
 			</li>
 			<li><a href="Uteliv.php"> Uteliv</a>
