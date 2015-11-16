b968b66a07cd813f067b8c890f767d57be875d56
Nomis <sschjeld@gmail.com>
2015-11-16 11:30:46 +0100

    Update Kontakt_oss.php

--- Websider/Kontakt_oss.php
+++ Websider/Kontakt_oss.php
@@ -12,24 +12,7 @@
 
 		<div class="backgroundImage">
 			<div class="content">
-				<script type="text/javascript">
-				function validateEmail(email) {
-					var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-					return re.test(email);
-				}
-				function validateForm() {
-						var e = document.forms["contactUs"]["mail"].value;
-						if (!validateEmail(e)) {
-								alert("Ugyldig e-postadresse!");
-								return false;
-						}
-						var t = document.forms["contactUs"]["comments"].value;
-						if(t==null || t=="") {
-								alert("Du må skrive noe!");
-								return false;
-						}
-				}
-				</script>
+				<script src="../prosedyrer/contactUsForm.js"> </script>
 				<h2> Kontakt oss </h2>
 				<form name="contactUs" onsubmit="return validateForm()" method="post">
 				<div id="email">
